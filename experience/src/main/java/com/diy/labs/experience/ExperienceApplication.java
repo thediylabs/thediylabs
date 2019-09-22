fae897c35b430c8af03df2d414c5f2da03c64a19
theblackathena <nicholaschin20@gmail.com>
2019-09-22 14:03:07 -0700

    Initialize experience API

---
+++ experience/src/main/java/com/diy/labs/experience/ExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.diy.labs.experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperienceApplication.class, args);
+	}
+
+}
---
+++ experience/src/test/java/com/diy/labs/experience/ExperienceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.diy.labs.experience;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExperienceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
