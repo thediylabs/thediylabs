283cb9a30e9e49823203ec1ec97f20a5a3c75da5
Alvin Lo <p8c1b@ugrad.cs.ubc.ca>
2019-11-02 22:48:25 -0700

    tdl-geolocation init

---
+++ tdl-geolocation/src/main/java/com/thediylabs/geolocation/model/GeoPoint.java
@@ -0,0 +1,23 @@
+package com.thediylabs.geolocation.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter 
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class GeoPoint {
+
+    @JsonProperty("latitude")
+    private String latitude;
+    @JsonProperty("longitude")
+    private String longitude;
+    
+}
---
+++ tdl-geolocation/src/main/java/com/thediylabs/geolocation/service/GeolocationService.java
@@ -0,0 +1,8 @@
+package com.thediylabs.geolocation.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GeolocationService {
+
+}
